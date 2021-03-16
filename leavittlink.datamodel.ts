54dadf0959ba15f9317919da9432310cd8c0dfb8
Programmer Build Server User <programmerbuildserveruser@leavitt.com>
2021-03-16 13:55:22 -0600

    Update auto-generated typescript file

--- leavittlink.datamodel.ts
+++ leavittlink.datamodel.ts
@@ -476,7 +476,7 @@ export interface HomeOwnersQuoteRequest extends QuoteRequest {
     RoofType: 'ALUMINUM_SHINGLES'|'ARCHITECTURAL_SHINGLES'|'ASBESTOS'|'ASPHALT_SHINGLE'|'CEDAR_SHAKES'|'CEDAR_SHINGLES'|'CLAY_TILE_OR_SLATE'|'COMPOSITION_FIBERGLASS_ASPHALT_ETC'|'CONCRETE_TILE'|'CONCRETE_NOT_TILE'|'COPPER'|'FIBERGLASS'|'FOAM'|'GRAVEL'|'METAL'|'MINERAL_FIBER'|'MISSION_TILE'|'OTHER'|'PLASTIC'|'POURED'|'RECYCLED_ROOFING_PRODUCTS'|'ROCK'|'ROLLED_ROOFING'|'RUBBER_ROOF'|'SLATE'|'SPANISH_TILE'|'CORRUGATED_STEEL'|'STEEL_PORCELAIN_SHINGLES'|'TAR_AND_GRAVEL'|'TILE'|'TIN'|'WOOD_FIBER_SHINGLES'|'WOOD_SHAKES'|'WOOD_SHINGLES'|'WOOD_SHAKE_SHINGLE' | null;
     SquareFootage: number | null;
     State: string | null;
-    Status: 'Step1'|'Step2'|'Step3'|'Step4'|'Step5'|'Step6';
+    Status: 'Step1'|'Step2'|'Step3'|'Step4'|'Step5'|'Step6'|'Step7';
     Street1: string | null;
     Street2: string | null;
     YearBuilt: number | null;
@@ -513,6 +513,15 @@ export interface AutoDiscountsDto {
     EducationLevel: 'NoHighSchoolDiplomaOrGed'|'HighSchoolDiplomaOrGed'|'VocationalTradeSchoolMilitary'|'CompletedSomeCollege'|'CurrentlyInCollege'|'CollegeDegree'|'GraduateWorkOrGraduateDegree' | null;
     EnrollTelematicsProgram: boolean;
     PaperLess: boolean;
+}
+
+export interface HomeOwnersStepSixDto {
+    HasAutomaticSprinklers: boolean | null;
+    HasBurglarAlarm: boolean | null;
+    HasDeadBolts: boolean | null;
+    HasFireAlarm: boolean | null;
+    HasFireExtinguisher: boolean | null;
+    HasSmokeDetector: boolean | null;
 }
 
 export interface HomeOwnersStepFiveDto {
